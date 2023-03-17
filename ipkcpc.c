6250778c8494f66815a9ec80895a47bd15c9a798
Ilsasas <cus.samuel@gmail.com>
2023-03-17 15:09:22 +0100

    Update ipkcpc.c
    Included header files and added general structure of the programe

---
+++ ipkcpc.c
@@ -0,0 +1,15 @@
+/*
+ * IPK project 1
+ * Samuel Cus (xcussa00@fit.vutbr.cz)
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+int main (int argc, const char * argv[])
+{
+    
+}
